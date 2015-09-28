3a8a02ac9eb31641712c7b8bfb51c1ecc229d7a9
mctavish141 <Serega@MacBook-Air.local>
2015-09-28 17:19:13 +0300

    Initial commit

---
+++ courses/prog_base/labs/lab1/lab1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lab1
+//
+//  Created by Serhii Kopach on 28.09.15.
+//  Copyright (c) 2015 Serhii Kopach. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
